9a308d752d50829c99d4dfc3bfa9b3d0f2403e5e
Miles Budden <git@miles.so>
2021-08-03 16:29:20 +0100

    Added some basic init code

--- ci_plumber/main.py
+++ ci_plumber/main.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 import typer
+from git import Repo
 
 app = typer.Typer()
 
@@ -6,21 +9,49 @@ app = typer.Typer()
 @app.callback()
 def callback() -> None:
     """
-    Awesome Portal Gun
+    CI Plumber
     """
 
 
 @app.command()
-def shoot() -> None:
+def init() -> None:
     """
-    Shoot the portal gun
+    Initialise the project
     """
-    typer.echo("Shooting portal gun")
+    typer.echo("Initialising app")
+
+    # Get the config directory
+    app_dir: str = typer.get_app_dir("CI-Plumber")
+    # Get the config file
+    config_path: Path = Path(app_dir) / "config.json"
+    # If it doesn't exist, create it
+    if not config_path.is_file():
+        typer.echo("Config file doesn't exist yet")
+        Path.mkdir(Path(app_dir), parents=True, exist_ok=True)
+        config_path.touch()
+        typer.echo("Created config file")
+    # Get the repo
+    repo = Repo(Path.cwd())
+    # Check it isn't bare
+    assert not repo.bare
+    # Get the remotes
+    remotes = repo.remotes
+
+    if len(remotes) == 1:
+        typer.echo("Found a remote")
+        # Get the remote
+        remote: str = remotes[0].url
+        remote
+    else:
+        # TODO Allow for multiple remotes
+        typer.echo("Found multiple remotes")
+        typer.Exit(1)
 
 
 @app.command()
-def load() -> None:
-    """
-    Load the portal gun
-    """
-    typer.echo("Loading portal gun")
+def pwd() -> None:
+    typer.echo(Path.cwd())
+
+
+if __name__ == "__main__":
+    app()
