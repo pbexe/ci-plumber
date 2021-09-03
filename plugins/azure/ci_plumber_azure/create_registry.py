d32fd64afd8ae7f5b0e28d053bcab2bf393cd1f1
Miles Budden <git@miles.so>
2021-09-03 15:40:49 +0100

    Remove hyphen

--- plugins/azure/ci_plumber_azure/create_registry.py
+++ plugins/azure/ci_plumber_azure/create_registry.py
@@ -27,7 +27,7 @@ class Skus(str, Enum):
 
 def create_registry(
     registry_name: str = typer.Option(
-        "registry-" + str(random.randint(100_000_000, 999_999_999)),
+        "registry" + str(random.randint(100_000_000, 999_999_999)),
         help="The name of the registry",
         prompt=True,
     ),
